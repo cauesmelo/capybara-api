f30dbf09548c148076e87fa3b4c661e59b29d5bf
Carolina Fantini <carolfantinirocha@gmail.com>
2022-05-15 12:57:20 -0300

    clean files

---
+++ Models/Base/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace capybara_api.Models.Base
+{
+    public class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string EditedBy { get; set; }
+        public DateTime EditedOn { get; set; }
+    }
+}
