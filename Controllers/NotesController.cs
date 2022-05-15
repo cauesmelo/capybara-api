ade8ed30bec0b0ef4c674dc5e58d906097a44bbc
Carolina Fantini <carolfantinirocha@gmail.com>
2022-05-15 15:15:03 -0300

    add scaffold for controllers

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using capybara_api.Infra;
+using capybara_api.Models;
+
+namespace capybara_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Notes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> GetNote()
+        {
+          if (_context.Note == null)
+          {
+              return NotFound();
+          }
+            return await _context.Note.ToListAsync();
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> GetNote(Guid id)
+        {
+          if (_context.Note == null)
+          {
+              return NotFound();
+          }
+            var note = await _context.Note.FindAsync(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return note;
+        }
+
+        // PUT: api/Notes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNote(Guid id, Note note)
+        {
+            if (id != note.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(note).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Note>> PostNote(Note note)
+        {
+          if (_context.Note == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Note'  is null.");
+          }
+            _context.Note.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNote", new { id = note.Id }, note);
+        }
+
+        // DELETE: api/Notes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNote(Guid id)
+        {
+            if (_context.Note == null)
+            {
+                return NotFound();
+            }
+            var note = await _context.Note.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _context.Note.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NoteExists(Guid id)
+        {
+            return (_context.Note?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
